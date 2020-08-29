afc902e8b95a9b2e010b8fb8eebcf3893a1af476
Allison Holt <alholt@akamai.com>
2020-08-29 16:35:55 -0400

    [master] Fetching data from server yay

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { CharacterList } from "./CharacterList";
-import { listCharacters } from "./character";
-
-listCharacters();
+const fetchTest = async () => {
+  const out = await fetch("http://localhost:8080/runs/all")
+    .then((response) => response.json())
+    .then((json) => json)
+    .catch((error) => {
+      console.log(error.toString());
+    });
+  return await out;
+};
 
 function App() {
+  const [output, setOutput] = useState({});
+
+  const getOutput = async () => {
+    let output = await fetchTest();
+    setOutput(output);
+  };
+
+  useEffect(() => {
+    getOutput();
+  }, []);
+
   return (
     <div className="App">
-      <CharacterList />
+      <h1>Runlog</h1>
+      <pre>waaah i want some {output.toString()}</pre>
     </div>
   );
 }
